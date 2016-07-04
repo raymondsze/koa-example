a5211c0d3852f9569da244c571834705feb14ada
Raymond Sze <raymond.sze@iheha.com>
2016-07-04 18:27:24 +0800

    update redis connect and config to 0.0.0.0

--- src/database/redis/connect.js
+++ src/database/redis/connect.js
@@ -14,7 +14,24 @@ if (username || password) {
 const uri = `redis://${prefix}${host}:${port}/${db}`;
 
 export default async (/* app */) => {
-  const client = redis.createClient(config.database.redis);
+  const client = redis.createClient(config.database.redis, {
+    retry_strategy: (options) => {
+      if (options.error.code === 'ECONNREFUSED') {
+        // End reconnecting on a specific error and flush all commands with a individual error
+        return new Error('The server refused the connection');
+      }
+      if (options.total_retry_time > 1000 * 60 * 60) {
+        // End reconnecting after a specific timeout and flush all commands with a individual error
+        return new Error('Retry time exhausted');
+      }
+      if (options.times_connected > 10) {
+        // End reconnecting with built in error
+        return undefined;
+      }
+      // reconnect after
+      return Math.max(options.attempt * 100, 5000);
+    },
+  });
   client.on('ready', () => {
     logger.info(`${uri} connected!`);
   });
